eaff0ebebc8299660ab70d1c178e1adbf980bc29
remeliora <remeliora2020@gmail.com>
2023-10-09 12:30:55 +1000

    Lesson_1

---
+++ src/main/java/com/example/sweater_1/Sweater1Application.java
@@ -0,0 +1,13 @@
+package com.example.sweater_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sweater1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sweater1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sweater_1/Sweater1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweater_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sweater1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
