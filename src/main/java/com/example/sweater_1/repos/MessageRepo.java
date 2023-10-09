795db12bd629bcf603125982aeabf65dde14c465
remeliora <remeliora2020@gmail.com>
2023-10-09 17:10:44 +1000

    Lesson_2

---
+++ src/main/java/com/example/sweater_1/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.sweater_1.repos;
+
+import com.example.sweater_1.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
